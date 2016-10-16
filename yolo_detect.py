6e6d6759c2ba3041814c3da35fdcfd824ead1d5b
Emanuele Plebani <banus80@gmail.com>
2016-10-16 14:08:52 +0200

    split further main and show_detections, enclose only label in box
    The main function only processes program options and delegates the rest
    to the function 'detect'. Separate function to draw a box on the image.
    In visualization, the white bar with detection details has been replaced
    by a reen box of the same size of the text.
    Rename yolo_main in yolo_detect.

--- yolo_detect.py
+++ yolo_detect.py
@@ -28,14 +28,12 @@ def get_boxes(output, img_size, grid_size, num_boxes):
     w_img, h_img = img_size[1], img_size[0]
     boxes = np.reshape(output, (grid_size, grid_size, num_boxes, 4))
 
-    offset = np.transpose(
-        np.reshape(np.array([np.arange(grid_size)]*grid_size*2),
-                   (num_boxes, grid_size, grid_size)), (1, 2, 0))
+    offset = np.tile(np.arange(grid_size)[:, np.newaxis], (grid_size, 1, num_boxes))
 
     boxes[:, :, :, 0] += offset
     boxes[:, :, :, 1] += np.transpose(offset, (1, 0, 2))
     boxes[:, :, :, 0:2] /= 7.0
-    # the prediction is the square root of the box size
+    # the predicted size is the square root of the box size
     boxes[:, :, :, 2:4] *= boxes[:, :, :, 2:4]
 
     boxes[:, :, :, [0, 2]] *= w_img
@@ -80,7 +78,7 @@ def get_candidate_objects(output, img_size):
 
     boxes, probs = parse_yolo_output(output, img_size)
 
-    filter_mat_probs = np.array(probs >= threshold, dtype='bool')
+    filter_mat_probs = (probs >= threshold)
     filter_mat_boxes = np.nonzero(filter_mat_probs)[0:3]
     boxes_filtered = boxes[filter_mat_boxes]
     probs_filtered = probs[filter_mat_probs]
@@ -91,6 +89,7 @@ def get_candidate_objects(output, img_size):
     probs_filtered = probs_filtered[idx]
     classes_num_filtered = classes_num_filtered[idx]
 
+    # Non-Maxima Suppression: greedily suppress low-score overlapped boxes
     for i, box_filtered in enumerate(boxes_filtered):
         if probs_filtered[i] == 0:
             continue
@@ -98,7 +97,7 @@ def get_candidate_objects(output, img_size):
             if iou(box_filtered, boxes_filtered[j]) > iou_threshold:
                 probs_filtered[j] = 0.0
 
-    filter_iou = np.array(probs_filtered > 0.0, dtype='bool')
+    filter_iou = (probs_filtered > 0.0)
     boxes_filtered = boxes_filtered[filter_iou]
     probs_filtered = probs_filtered[filter_iou]
     classes_num_filtered = classes_num_filtered[filter_iou]
@@ -112,19 +111,33 @@ def get_candidate_objects(output, img_size):
 
 def iou(box1, box2):
     """ compute intersection over union score """
-    box_tb = min(box1[0]+0.5*box1[2], box2[0]+0.5*box2[2]) - \
+    int_tb = min(box1[0]+0.5*box1[2], box2[0]+0.5*box2[2]) - \
              max(box1[0]-0.5*box1[2], box2[0]-0.5*box2[2])
-    box_lr = min(box1[1]+0.5*box1[3], box2[1]+0.5*box2[3]) - \
+    int_lr = min(box1[1]+0.5*box1[3], box2[1]+0.5*box2[3]) - \
              max(box1[1]-0.5*box1[3], box2[1]-0.5*box2[3])
-    intersection = max(0.0, box_tb) * max(0.0, box_lr)
+    intersection = max(0.0, int_tb) * max(0.0, int_lr)
 
     return intersection / (box1[2]*box1[3] + box2[2]*box2[3] - intersection)
 
 
-def show_results(img, results, img_size):
+def draw_box(img, name, box, score):
+    """ draw a single bounding box on the image """
+    xmin, ymin, xmax, ymax = box
+
+    box_tag = '{} : {:.2f}'.format(name, score)
+    text_x, text_y = 5, 7
+
+    cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+    boxsize, _ = cv2.getTextSize(box_tag, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    cv2.rectangle(img, (xmin, ymin-boxsize[1]-text_y),
+                  (xmin+boxsize[0]+text_x, ymin), (0, 225, 0), -1)
+    cv2.putText(img, box_tag, (xmin+text_x, ymin-text_y),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+
+
+def show_results(img, results):
     """ draw bounding boxes on the image """
-    img_cp = img.copy()
-    img_width, img_height = img_size[1], img_size[0]
+    img_width, img_height = img.shape[1], img.shape[0]
     disp_console = True
     imshow = True
 
@@ -137,14 +150,30 @@ def show_results(img, results, img_size):
         ymin, ymax = max(box_y-box_h//2, 0), min(box_y+box_h//2, img_height)
 
         if imshow:
-            cv2.rectangle(img_cp, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
-            cv2.rectangle(img_cp, (xmin, ymin-20), (xmax, ymin), (125, 125, 125), -1)
-            cv2.putText(img_cp, result[0] + ' : %.2f' % result[5], (xmin+5, ymin-7),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+            draw_box(result[0], (xmin, ymin, xmax, ymax), result[5])
     if imshow:
-        cv2.imshow('YOLO detection', img_cp)
-        cv2.waitKey(1000)
+        cv2.imshow('YOLO detection', img)
 
+
+def detect(model_filename, weight_filename, img_filename):
+    """ given a YOLO caffe model and an image, detect the objects in the image
+    """
+    net = caffe.Net(model_filename, weight_filename, caffe.TEST)
+    img = caffe.io.load_image(img_filename) # load the image using caffe.io
+
+    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+    transformer.set_transpose('data', (2, 0, 1))
+    transformer.set_channel_swap('data', (2, 1, 0))
+
+    t_start = datetime.now()
+    out = net.forward_all(data=np.asarray([transformer.preprocess('data', img)]))
+    t_end = datetime.now()
+    print('total time is {:.2f} milliseconds'.format((t_end-t_start).total_seconds()*1e3))
+
+    img_cv = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    results = get_candidate_objects(out['result'][0], img.shape)
+    show_results(img_cv, results)
+    cv2.waitKey()
 
 
 def main(argv):
@@ -152,45 +181,33 @@ def main(argv):
     model_filename = ''
     weight_filename = ''
     img_filename = ''
+    usage_message = 'Usage: yolo_detect.py -m <model_file> -w <output_file> -i <img_file>'
+
     try:
         opts, _ = getopt.getopt(argv, "hm:w:i:")
         print(opts)
     except getopt.GetoptError:
-        print('yolo_main.py -m <model_file> -w <output_file> -i <img_file>')
+        print(usage_message)
+        return
+    if not opts:
+        print(usage_message)
         return
 
     for opt, arg in opts:
         if opt == '-h':
-            print('yolo_main.py -m <model_file> -w <weight_file> -i <img_file>')
-            sys.exit()
+            print(usage_message)
+            return
         elif opt == "-m":
             model_filename = arg
         elif opt == "-w":
             weight_filename = arg
         elif opt == "-i":
             img_filename = arg
-    print('model file is "', model_filename)
-    print('weight file is "', weight_filename)
-    print('image file is "', img_filename)
+    print('model file is {}'.format(model_filename))
+    print('weight file is {}'.format(weight_filename))
+    print('image file is {}'.format(img_filename))
 
-    net = caffe.Net(model_filename, weight_filename, caffe.TEST)
-    img = caffe.io.load_image(img_filename) # load the image using caffe io
-    inputs = img
-    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-    transformer.set_transpose('data', (2, 0, 1))
-    transformer.set_channel_swap('data', (2, 1, 0))
-    start = datetime.now()
-    out = net.forward_all(data=np.asarray([transformer.preprocess('data', inputs)]))
-    end = datetime.now()
-    elapsed_time = end-start
-    print('total time is {:.02f} milliseconds'.format(elapsed_time.total_seconds()*1e3))
-
-    img_cv = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    # fc27 instead of fc12 for yolo_small
-    results = get_candidate_objects(out['result'][0], img.shape)
-    show_results(img_cv, results, img.shape)
-    cv2.waitKey(10000)
-
+    detect(model_filename, weight_filename, img_filename)
 
 
 if __name__ == '__main__':
